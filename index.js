64ee79367bb816089223cd7a4ea0d8982bc4c8bf
kskanak <kskanak26@gmail.com>
2022-11-27 02:35:18 +0600

    allseller api created

--- index.js
+++ index.js
@@ -23,17 +23,48 @@ async function run() {
   const catagoryItemsCollection = client
     .db("alikeNew")
     .collection("catagoryItems");
-
-  app.get("/catagoryItems/:id", async (req, res) => {
-    const id = parseInt(req.params.id);
-    const query = {
-      catagory_id: id,
-    };
-    const result = await catagoryItemsCollection.find(query).toArray();
-    res.send(result);
-  });
+  const userCollection = client.db("alikeNew").collection("users");
+  const bookingCollection = client.db("alikeNew").collection("bookings");
 
   try {
+    app.get("/catagoryItems/:id", async (req, res) => {
+      const id = parseInt(req.params.id);
+      const query = {
+        catagory_id: id,
+      };
+      const result = await catagoryItemsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // user api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email };
+
+      const existedUser = await userCollection.findOne(query);
+      if (existedUser) {
+        res.send({ acknowledged: "existed" });
+        return;
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // item booking api
+    app.post("/bookings", async (req, res) => {
+      const item = req.body;
+      const result = await bookingCollection.insertOne(item);
+      res.send(result);
+    });
+
+    // all seller api
+
+    app.get("/allseller", async (req, res) => {
+      const query = { userRole: "seller" };
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
