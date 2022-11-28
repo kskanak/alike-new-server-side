8d997a19a50bff24accc64f62246223c81dcaca2
kskanak <kskanak26@gmail.com>
2022-11-29 02:51:46 +0600

    seller product delete api created

--- index.js
+++ index.js
@@ -117,6 +117,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete Seller products
+    app.delete("/deleteSellerProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await catagoryItemsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // all buyer api
     app.get("/allbuyer", async (req, res) => {
       const query = { userRole: "buyer" };
