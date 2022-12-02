7c67bff44a964806196304fd58a8dd8f24e7483f
kskanak <kskanak26@gmail.com>
2022-12-02 09:47:14 +0600

    coded

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // middle ware
 app.use(cors());
